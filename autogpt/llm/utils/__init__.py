c2dab8bca1e56be0e197b9e988d2b87acba2f52c
Danny <weilong@dao42.com>
2023-06-07 00:13:51 +0800

    refact: skip check_model

--- autogpt/llm/utils/__init__.py
+++ autogpt/llm/utils/__init__.py
@@ -250,6 +250,7 @@ def create_chat_completion(
 def check_model(
     model_name: str, model_type: Literal["smart_llm_model", "fast_llm_model"]
 ) -> str:
+    return model_name
     """Check if model is available for use. If not, return gpt-3.5-turbo."""
     api_manager = ApiManager()
     models = api_manager.get_models()
